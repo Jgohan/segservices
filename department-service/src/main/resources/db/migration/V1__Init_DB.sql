c7c1e87688bb95169c5523f73c726a718d07c57f
Roman Tarasov <pv.pwp.roma@yandex.ru>
2022-09-06 22:55:51 +0400

    Refactored migration files

--- department-service/src/main/resources/db/migration/V1__Init_DB.sql
+++ department-service/src/main/resources/db/migration/V1__Init_DB.sql
@@ -2,3 +2,7 @@ create table departments(
     id uuid not null primary key,
     name varchar(255) not null
 );
+
+insert into departments (id, name) values
+    ('4373788d-fde1-4ce4-a103-f6111dc88af1', 'dep1'),
+    ('4373788d-fde1-4ce4-a103-f6111dc88af2', 'dep2')
